d4a2bea855e15cc71255af7a60937d5729d8ffca
Alonso Vazquez <aoavzm@gmail.com>
2022-12-19 15:52:40 -0700

    skeleton

---
+++ app/api/session.py
@@ -0,0 +1,55 @@
+from flask import Blueprint, request, jsonify
+from flask_login import login_required, current_user, login_user, logout_user
+from app.models import db, User, Post, Follower
+
+
+user_routes = Blueprint('users', __name__)
+
+
+# Get Current User
+@user_routes.route('/')
+def session():
+    user = current_user
+    return user.to_dict()
+
+
+# Sign Up User
+@user_routes.route('/signup', methods=["POST"])
+def signup():
+    user_info = request.json
+    user_by_email = User.query.filter((User.email == user_info["email"])).first()
+    if user_by_email:
+        return { "errors": ["User with this email already exists"] }, 403
+    user_by_username = User.query.filter((User.username == user_info["username"])).first()
+    if user_by_username:
+        return { "errors": ["User with this username already exists"] }, 403
+    new_user = User(**user_info)
+    db.session.add(new_user)
+    db.session.commit()
+    login_user(new_user)
+    return new_user.safe_info()
+
+
+# Sign In User
+@user_routes.route('/signin', methods=["POST"])
+def login():
+    user_info = request.json
+    credential = user_info["credential"]
+    password = user_info["password"]
+    user = User.query.filter((User.email == credential) | (User.username == credential)).first()
+    if not user:
+        return { "errors": ["Invalid credential"] }, 401
+    if not user.check_password(password):
+        return { "errors": ["Invalid password"] }, 401
+    login_user(user)
+    return user.safe_info()
+
+
+# Log Out User
+@user_routes.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return {
+        "message": "Logged out successfully."
+    }
